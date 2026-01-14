e77af143e34b0fa3be2a68936f3d4e520c212429
Shivam Upadhyay <shivamupadhyay2329@gmail.com>
2026-01-14 10:55:19 +0530

    Lecture_43

---
+++ Lecture_43/function.js
@@ -0,0 +1,28 @@
+// function sayName(){
+//     console.log("Shivam Upadhyay")
+// } 
+
+// sayName();
+
+// function printCounting(){
+//     for(let i=0;i<=100;i++){
+//         console.log(i);
+//     }
+// }
+
+// printCounting();
+
+// function printNumber(num){
+//     console.log("Printing Number: ",num);
+// }
+
+// printNumber(10);
+
+// function getSum(a,b,c){
+//     let sum=a+b+c;
+//     return sum;
+// }
+
+// console.log("Sum is:",getSum(4,7,10));
+  
+  
